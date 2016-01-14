a4da864d54707fa257bb893212417b2dab81b49f
rosa <r215130@gmail.com>
2016-01-14 17:42:15 -0500

    giving funcionality to the sign in

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,25 @@
 
   // SHOW MENU SIGN IN
 $('.b-sign').on('click', function(){
-  $('.sign-form').slideDown('slow');
+  //$('.sign-form').slideDown('slow');
+  //$('.sign-form').slideToggle();
+  //$('.sign-form').animate({
+    //opacity: 0.75,
+    //bottom: "0",
+    //height: "toggle"
+  //},1000, function() {
+   // Animation complete.
+  //});
+  //$('.sign-form').slideToggle();
+
+  $('.sign-form').show().animate({top:'-=200px', opacity:'1'}, 1000);//works for left and right but does not for up
+
+  //$('.sign-form').show('slide',{direction:'up'}, 100);//.sign-form apprears but .b-sign does not desappear
+
   $('.b-sign').hide();
+  //$('.b-sign').slideUp(); remove the sign in button motion up
   console.log("testing form");
+
 });
 
 })(); //END OF IIFE
