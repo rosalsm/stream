311066bc329f46539bff6d2fb816d4d832092313
rosa <r215130@gmail.com>
2016-01-15 16:57:14 -0500

    style and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -13,14 +13,23 @@ $('.b-sign').on('click', function(){
   //});
   //$('.sign-form').slideToggle();
 
-  $('.sign-form').show().animate({top:'-=200px', opacity:'1'}, 1000);//works for left and right but does not for up
-
+  $('.sign-form').show().animate({top:'-=250px', opacity:'1'}, 1000)//works for left and right but does not for up
+  console.log("test 1")
+  $('.main-title, .big-s').animate({top:'-=50px'}, 1000)
+  console.log("test 2")
   //$('.sign-form').show('slide',{direction:'up'}, 100);//.sign-form apprears but .b-sign does not desappear
 
   $('.b-sign').hide();
   //$('.b-sign').slideUp(); remove the sign in button motion up
   console.log("testing form");
+});
+
+$('.container').on("swipe", function(){
+  $('.sign-form2').show().animate({top:'-=250px'}, 1000)
+  console.log("testing swipe")
+  $('.b-sign').hide();
 
 });
+
 
 })(); //END OF IIFE
